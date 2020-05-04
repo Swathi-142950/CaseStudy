a328aa8442996cb93ad9395d9f872f46efc6d234
Swathi-142950 <64530616+Swathi-142950@users.noreply.github.com>
2020-05-04 21:17:38 +0530

    Update app.module.ts

--- CaseStudyWeb/src/app/app.module.ts
+++ CaseStudyWeb/src/app/app.module.ts
@@ -1,18 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser'
 import { AppComponent } from './app.component';
+import { CustomerComponent } from './bookCarWash/customer.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router'
+import { AboutUsComponent } from './aboutUs/about.us.component';
+import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signUp/signup.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    imports: [BrowserModule, FormsModule,ReactiveFormsModule, RouterModule.forRoot([
+        {path: 'home', component: HomeComponent},
+        {path: 'login', component: LoginComponent},
+        {path: 'signup', component: SignupComponent},
+        {path: 'customer', component: CustomerComponent},
+        {path: '', redirectTo: 'home', pathMatch:'full'}
+    ]),RouterModule.forChild([
+        {path: 'aboutUs', component: AboutUsComponent, outlet: 'secondary'}
+    ]),HttpClientModule],
+    declarations: [AppComponent, HomeComponent, AboutUsComponent, CustomerComponent, LoginComponent, SignupComponent],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
