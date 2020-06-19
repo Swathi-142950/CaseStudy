f80def9caea36091111009e952315ed564caf8f2
Swathi04 <swathianandram@gmail.com>
2020-06-19 13:04:29 +0530

    add imports

--- CaseStudyWeb/src/app/app.module.ts
+++ CaseStudyWeb/src/app/app.module.ts
@@ -13,12 +13,11 @@ import { CarwashComponent } from './bookCarWash/carwash.component';
 import { PaymentComponent } from './bookCarWash/payment/payment.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatDatepickerModule, MatInputModule, MatNativeDateModule } from '@angular/material';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { AdminPanelComponent } from './adminPanel/admin.panel.component';
 import { AppRouteModule } from './app.route.module';
 import { AdminPanelModule } from './adminPanel/admin.panel.module';
 import { AuthGuard } from './security/auth.guard';
-import { AuthServiceConfig, SocialLoginModule, AuthService, FacebookLoginProvider } from 'angularx-social-login'
+import { AuthServiceConfig, AuthService, FacebookLoginProvider } from 'angularx-social-login'
 import { WasherComponent } from './washerPanel/washer.component';
 export function socialConfigs() {
     const config = new AuthServiceConfig(
@@ -32,9 +31,9 @@ export function socialConfigs() {
     return config;
 }
 @NgModule({
-    imports: [BrowserModule, FormsModule, MatDatepickerModule, MatInputModule, MatSlideToggleModule, AdminPanelModule, MatNativeDateModule, ReactiveFormsModule, AppRouteModule, RouterModule.forChild([
+    imports: [BrowserModule, FormsModule, MatDatepickerModule,BrowserAnimationsModule, MatInputModule, AdminPanelModule, MatNativeDateModule, ReactiveFormsModule, AppRouteModule, RouterModule.forChild([
         { path: 'aboutUs', component: AboutUsComponent, outlet: 'secondary' }
-    ]), HttpClientModule, BrowserAnimationsModule],
+    ]), HttpClientModule],
     declarations: [AppComponent,
         HomeComponent,
         AdminPanelComponent,
